f915e24379b828937c44b05a2599efc7f5d3f791
Erdem Yigit Ertorer <eertorer@andrew.cmu.edu>
2025-01-28 19:28:58 -0500

    Update train_ECON_AE_CAE.py

--- train_ECON_AE_CAE.py
+++ train_ECON_AE_CAE.py
@@ -4,6 +4,15 @@ import numpy as np
 import pandas as pd
 
 import tensorflow as tf
+
+SEED = 12345
+os.environ["PYTHONHASHSEED"] = str(SEED)
+random.seed(SEED)
+np.random.seed(SEED)
+tf.random.set_seed(SEED)
+tf.config.experimental.enable_op_determinism(True)  # optional if TF supports it
+
+
 gpus = tf.config.list_physical_devices('GPU')
 if gpus:
     try:
